3c133525179dde545d76b024406ce441551bd080
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-05-21 18:56:28 +0200

    discover use of polyglot macros via cargo build script

---
+++ examples/wasm-add/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    polyglot::build().unwrap();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 pub extern crate wasmtime;
 
+use glob::glob;
+use regex::Regex;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
 #[macro_export]
 macro_rules! go {
     ($mod_name:ident $ref:expr) => {
@@ -32,4 +39,47 @@ macro_rules! wasm {
             }
         }
     };
+}
+
+#[derive(Default)]
+pub struct BuildOptions {}
+
+pub fn build() -> Result<(), Box<dyn Error>> {
+    build_with(&std::env::current_dir()?, &BuildOptions::default())
+}
+
+pub fn build_with(root: &std::path::Path, _options: &BuildOptions) -> Result<(), Box<dyn Error>> {
+    // TODO: find a more robust approach
+    let polyglot_re = Regex::new(r#"^polyglot::([^!]+)!\(([^ ]+) "([^)]+)"\);$"#).unwrap();
+
+    let pattern = format!("{}/**/*.rs", root.to_str().unwrap());
+
+    for filepath in glob(&pattern).unwrap() {
+        let filepath = filepath?;
+
+        // TODO: leverage .gitignore for this
+        let entry_str = filepath.to_str().unwrap();
+        if entry_str.ends_with("/build.rs") || entry_str.contains("/target/") {
+            continue;
+        }
+
+        let handle = File::open(&filepath)?;
+        let lines = BufReader::new(handle).lines();
+        for line in lines {
+            let dir = Path::parent(&filepath).unwrap();
+
+            if let Some(captures) = polyglot_re.captures(&line?) {
+                let language = &captures[1];
+                let mod_name = &captures[2];
+                let relative_path = &captures[3];
+                let absolute_path = dir.join(relative_path);
+                println!(
+                    "{} {} {} {:?}",
+                    language, mod_name, relative_path, absolute_path
+                );
+            }
+        }
+    }
+
+    Ok(())
 }
